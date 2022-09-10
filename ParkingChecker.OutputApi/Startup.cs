2f52ac5c5928f9917479916ee7a1685b3160938d
deced <samofalov.v@tut.by>
2022-09-10 21:53:06 +0300

    upd

--- ParkingChecker.OutputApi/Startup.cs
+++ ParkingChecker.OutputApi/Startup.cs
@@ -17,8 +17,16 @@ namespace ParkingChecker.OutputApi
 
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            
+            services.AddCors(options =>
+            {
+                options.AddPolicy("EnableCORS", builder =>
+                {
+                    builder.WithOrigins().AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(origin => true) // allow any origin
+                        .AllowCredentials().Build(); 
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
