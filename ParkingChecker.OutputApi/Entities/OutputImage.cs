1b70f737c42dac45017cc4f08746d9a6d2f9705b
tranquility09 <derri5437@gmail.com>
2022-09-18 15:22:03 +0300

    upd

---
+++ ParkingChecker.OutputApi/Entities/OutputImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MongoDbGenericRepository.Attributes;
+using ParkingChecker.OutputApi.Base.DataAccess;
+
+namespace ParkingChecker.OutputApi.Entities
+{
+    [CollectionName("output_image")]
+    public class OutputImage: Document
+    {
+        public string  fullPath { get; set; }
+        public string parkingId { get; set; }
+     
+    }
+}
